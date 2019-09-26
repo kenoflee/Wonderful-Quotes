e06ce0f833c359e86fad527b4b8e0a05bc1d3750
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-09-25 23:28:46 -0700

    passing data using props and slots

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="container">
-    
+        <app-quote-card :quotes="quotes"></app-quote-card>
     </div>
 </template>
 
 <script>
+    import QuoteCard from './components/QuoteCard.vue';
+
     export default {
-        
+        data() {
+            return {
+                quotes: ['How do you like them apple?'],
+                maxQuotes: 10,
+            }
+        },
+        components: {
+            appQuoteCard: QuoteCard,
+        }
     }
 </script>
 
---
+++ src/components/QuoteCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <app-quote :key="index" v-for="(quote, index) in quotes" > {{quote}} </app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote,
+        }
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
