32206a95751ff2563f822209a16184b78d16966c
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-09-26 18:56:07 -0700

    add NewQuote with props and components

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <div  class="col-sm-8 col-sm-off-2 col-xs-12 col-md-6 col-md-offset-3">
+
+        </div >
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote,
+        }
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
